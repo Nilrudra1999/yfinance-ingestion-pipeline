487a15b371bcd9f3501bfd3adbafe76c583648b3
Nilrudra1999 <nilrudram@gmail.com>
2026-08-25 23:36:43 -0400

    updating run_remote_etl.sh and requirements.txt to fix GH Action failure

--- run_remote_etl.sh
+++ run_remote_etl.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+# Ensure Git Bash can locate Windows installed binaries
+export PATH=$PATH:"/c/Program Files/GitHub CLI/":"/c/Program Files/Python311/":"/c/Program Files/Python311/Scripts/"
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$PROJECT_DIR"
-
 TICKER_FILE="tickers.txt"
 if [ ! -f "$TICKER_FILE" ]; then
     echo "Error: $TICKER_FILE does not exist in $PROJECT_DIR"
@@ -29,7 +30,10 @@ RUN_ID=$(gh run list --workflow=run_etl.yml --limit 1 --json databaseId -q '.[0]
 # Watch execution until completion
 gh run watch "$RUN_ID"
 echo "Downloading extracted artifact from GitHub..."
-gh run download "$RUN_ID" --name transformed-etl-data --dir . --clobber
+
+# Clean up any existing local JSON output before downloading
+rm -f transformed_data.json
+gh run download "$RUN_ID" --name transformed-etl-data --dir .
 echo "Loading data into Local MS SQL Express..."
 
 # Activate local virtual environment if present
